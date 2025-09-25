a6ae1e9083462f17109754266d510e1c8da4a129
Swanand8080 <66907612+Swanand8080@users.noreply.github.com>
2025-09-25 16:33:27 +0530

    Update and rename test to main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "myinstance1" {
+        ami = var.map1["ami"]
+        key_name = "Swanand_22_09_2025"
+        instance_type = var.map1["i-type"]
+        availability_zone = var.map1["az"]
+        tags = {
+                Name = "Swanand_instance"
+        }
+}
+
+resource "aws_ebs_volume" "vol1" {
+        availability_zone = var.map1["az"]
+        size              = var.map1["ebs-size"]
+        tags = {
+                Name = "Terraform_vol_1"
+        }
+}
+
+resource "aws_volume_attachment" "ebs_attach" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.vol1.id
+  instance_id = aws_instance.myinstance1.id
+}
